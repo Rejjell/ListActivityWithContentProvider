3cf3170e538a558ccef800bcde1febdc73f7be56
rummezheni <Maxim.Mezheninov@SymphonyTeleca.com>
2015-04-21 11:08:21 +0300

    ListActivity with Loader

--- app/src/main/java/com/example/rummezheni/price_history/MyListActivity.java
+++ app/src/main/java/com/example/rummezheni/price_history/MyListActivity.java
@@ -1,11 +1,15 @@
 package com.example.rummezheni.price_history;
 
 import android.app.ListActivity;
-import android.content.Context;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
-import android.database.DataSetObserver;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -13,62 +17,122 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.SimpleCursorAdapter;
-import android.widget.Toast;
+import android.provider.ContactsContract.Contacts;
 
-import java.util.ArrayList;
+public class MyListActivity extends ListActivity
+        implements SearchView.OnQueryTextListener, LoaderManager.LoaderCallbacks<Cursor> ,AdapterView.OnItemLongClickListener{
 
-/**
- * Created by rummezheni on 17.04.2015.
- */
-public class MyListActivity extends ListActivity implements  AdapterView.OnItemLongClickListener{
-
-    ArrayList<Integer> idList = new ArrayList<Integer>();
+    // This is the Adapter being used to display the list's data.
     SimpleCursorAdapter mAdapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState){
+    // If non-null, this is the current filter the user has provided.
+    String mCurFilter;
+
+    static String TAG = "TAG";
+
+    @Override public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Give some text to display if there is no data.  In a real
+        // application this would come from a resource.
+        //setEmptyText("No phone numbers");
+
+        // We have a menu item to show in action bar.
+        //setHasOptionsMenu(true);
+
+        // Create an empty adapter we will use to display the loaded data.
+        mAdapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_2, null,
+                new String[] {MyContentProvider.ITEM_NAME, MyContentProvider.ITEM_PRICE },
+                new int[] { android.R.id.text1, android.R.id.text2 }, 0);
+        setListAdapter(mAdapter);
+
+        // Prepare the loader.  Either re-connect with an existing one,
+        // or start a new one.
         this.getListView().setLongClickable(true);
         this.getListView().setOnItemLongClickListener(this);
-        bindAdapter();
+        getLoaderManager().initLoader(0, null, this);
+
     }
 
-    public void bindAdapter()
-    {
-        String[] from = new String[] {MyContentProvider.ITEM_NAME, MyContentProvider.ITEM_PRICE};
-        int[] to =  new int[] {android.R.id.text1, android.R.id.text2};
-        //Creating cursor
-        Cursor queryCursor = this.getContentResolver().query(MyContentProvider.CONTENT_URI,null,null,null,null);
-
-        if (queryCursor != null) {
-            //setting notithcation uri for created cursor
-            queryCursor.setNotificationUri(this.getContentResolver(),
-                    MyContentProvider.CONTENT_URI);
-            mAdapter = new SimpleCursorAdapter(this, android.R.layout.two_line_list_item, queryCursor, from, to, 0);
-            setListAdapter(mAdapter);
-            queryCursor.moveToFirst();
-            idList = new ArrayList<Integer>(queryCursor.getCount());
-            do {
-                if (queryCursor.getCount() > 0)
-                    idList.add(queryCursor.getInt(queryCursor.getColumnIndex(MyContentProvider.ITEM_ID)));
-            }
-            while (queryCursor.moveToNext());
-        }
-    }
-
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        Log.d("onList item click","on list item click");
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        // Place an action bar item for searching.
+        /*MenuItem item = menu.add("Search");
+        item.setIcon(android.R.drawable.ic_menu_search);
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        SearchView sv = new SearchView(this);
+        sv.setOnQueryTextListener(this);
+        item.setActionView(sv);*/
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_item_list, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onQueryTextChange(String newText) {
+        // Called when the action bar search text has changed.  Update
+        // the search filter, and restart the loader to do a new query
+        // with this filter.
+        mCurFilter = !TextUtils.isEmpty(newText) ? newText : null;
+        getLoaderManager().restartLoader(0, null, this);
+        return true;
+    }
+
+    @Override public boolean onQueryTextSubmit(String query) {
+        // Don't care about this.
+        return true;
+    }
+
+    @Override public void onListItemClick(ListView l, View v, int position, long id) {
+        // Insert desired behavior here.
+        Log.i("FragmentComplexList", "Item clicked: " + id);
+    }
+
+    // These are the Contacts rows that we will retrieve.
+    static final String[] ITEMS_SUMMARY_PROJECTION = new String[] {
+            MyContentProvider.ITEM_ID,
+            MyContentProvider.ITEM_NAME,
+            MyContentProvider.ITEM_PRICE,
+    };
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Log.d(TAG,"onLoadFinished fired");
+        // This is called when a new Loader needs to be created.  This
+        // sample only has one Loader, so we don't care about the ID.
+        // First, pick the base URI to use depending on whether we are
+        // currently filtering.
+        Uri baseUri;
+        if (mCurFilter != null) {
+            baseUri = Uri.withAppendedPath(Contacts.CONTENT_FILTER_URI,
+                    Uri.encode(mCurFilter));
+        } else {
+            baseUri = Contacts.CONTENT_URI;
+        }
+
+        // Now create and return a CursorLoader that will take care of
+        // creating a Cursor for the data being displayed.
+        String select = "((" + Contacts.DISPLAY_NAME + " NOTNULL) AND ("
+                + Contacts.HAS_PHONE_NUMBER + "=1) AND ("
+                + Contacts.DISPLAY_NAME + " != '' ))";
+
+        return new CursorLoader(this, MyContentProvider.CONTENT_URI,
+                ITEMS_SUMMARY_PROJECTION, null, null,
+                null);
+    }
+
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Swap the new cursor in.  (The framework will take care of closing the
+        // old cursor once we return.)
+        Log.d(TAG,"onLoadFinished fired");
+        mAdapter.swapCursor(data);
+    }
+
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // This is called when the last Cursor provided to onLoadFinished()
+        // above is about to be closed.  We need to make sure we are no
+        // longer using it.
+        Log.d(TAG,"onLoaderReset fired");
+        mAdapter.swapCursor(null);
     }
 
     @Override
@@ -76,42 +140,21 @@ public class MyListActivity extends ListActivity implements  AdapterView.OnItemL
         // Handle item selection
         switch (item.getItemId()) {
             case R.id.action_add:
-                showAddActivity();
+                Intent intent = new Intent(this, AddActivity.class);
+                startActivity(intent);
                 return true;
             case R.id.action_settings:
-                showSettings();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    private void showSettings() {
-    }
-
-    private void showAddActivity() {
-        Intent intent = new Intent(this, AddActivity.class);
-        startActivity(intent);
-    }
-
     @Override
     public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-        String selectedItem = parent.getItemAtPosition(position).toString();
-
-        Toast.makeText(getApplicationContext(),
-                selectedItem + " deleted.",
-                Toast.LENGTH_SHORT).show();
-        int itemId = idList.get(position);
-        getContentResolver().delete(MyContentProvider.CONTENT_URI, MyContentProvider.ITEM_ID + "=?", new String[]{"" + itemId});
-        //TODO change udate with Data Set Observer
-        //========
-        //mAdapter.notifyDataSetChanged();
-        //mAdapter.swapCursor(this.getContentResolver().query(MyContentProvider.CONTENT_URI, null, null, null, null));
-        //========
-
+        getContentResolver()
+                .delete(MyContentProvider.CONTENT_URI,MyContentProvider.ITEM_ID+"=?",new String[]{""+id});
+        Log.d(TAG,"onItemLongClick fired");
         return true;
-
     }
-
-
 }
