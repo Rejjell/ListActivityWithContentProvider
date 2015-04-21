9926e5c487c83f9550e27033248b843a1910f535
rummezheni <Maxim.Mezheninov@SymphonyTeleca.com>
2015-04-21 11:33:33 +0300

    Loader usage

--- app/src/main/java/com/example/rummezheni/price_history/MyListActivity.java
+++ app/src/main/java/com/example/rummezheni/price_history/MyListActivity.java
@@ -1,15 +1,13 @@
 package com.example.rummezheni.price_history;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -17,12 +15,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.SearchView;
 import android.widget.SimpleCursorAdapter;
-import android.provider.ContactsContract.Contacts;
 
 public class MyListActivity extends ListActivity
-        implements SearchView.OnQueryTextListener, LoaderManager.LoaderCallbacks<Cursor> ,AdapterView.OnItemLongClickListener{
+        implements LoaderManager.LoaderCallbacks<Cursor> ,AdapterView.OnItemLongClickListener{
 
     // This is the Adapter being used to display the list's data.
     SimpleCursorAdapter mAdapter;
@@ -40,13 +36,14 @@ public class MyListActivity extends ListActivity
         //setEmptyText("No phone numbers");
 
         // We have a menu item to show in action bar.
-        //setHasOptionsMenu(true);
-
         // Create an empty adapter we will use to display the loaded data.
         mAdapter = new SimpleCursorAdapter(this,
                 android.R.layout.simple_list_item_2, null,
                 new String[] {MyContentProvider.ITEM_NAME, MyContentProvider.ITEM_PRICE },
                 new int[] { android.R.id.text1, android.R.id.text2 }, 0);
+
+
+
         setListAdapter(mAdapter);
 
         // Prepare the loader.  Either re-connect with an existing one,
@@ -57,36 +54,22 @@ public class MyListActivity extends ListActivity
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getLoaderManager().restartLoader(0,null,this);
+    }
+
     @Override public boolean onCreateOptionsMenu(Menu menu) {
         // Place an action bar item for searching.
-        /*MenuItem item = menu.add("Search");
-        item.setIcon(android.R.drawable.ic_menu_search);
-        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
-        SearchView sv = new SearchView(this);
-        sv.setOnQueryTextListener(this);
-        item.setActionView(sv);*/
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_item_list, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
-    public boolean onQueryTextChange(String newText) {
-        // Called when the action bar search text has changed.  Update
-        // the search filter, and restart the loader to do a new query
-        // with this filter.
-        mCurFilter = !TextUtils.isEmpty(newText) ? newText : null;
-        getLoaderManager().restartLoader(0, null, this);
-        return true;
-    }
-
-    @Override public boolean onQueryTextSubmit(String query) {
-        // Don't care about this.
-        return true;
-    }
-
     @Override public void onListItemClick(ListView l, View v, int position, long id) {
         // Insert desired behavior here.
-        Log.i("FragmentComplexList", "Item clicked: " + id);
+        Log.i(TAG, "Item clicked: " + id);
     }
 
     // These are the Contacts rows that we will retrieve.
@@ -101,20 +84,6 @@ public class MyListActivity extends ListActivity
         // sample only has one Loader, so we don't care about the ID.
         // First, pick the base URI to use depending on whether we are
         // currently filtering.
-        Uri baseUri;
-        if (mCurFilter != null) {
-            baseUri = Uri.withAppendedPath(Contacts.CONTENT_FILTER_URI,
-                    Uri.encode(mCurFilter));
-        } else {
-            baseUri = Contacts.CONTENT_URI;
-        }
-
-        // Now create and return a CursorLoader that will take care of
-        // creating a Cursor for the data being displayed.
-        String select = "((" + Contacts.DISPLAY_NAME + " NOTNULL) AND ("
-                + Contacts.HAS_PHONE_NUMBER + "=1) AND ("
-                + Contacts.DISPLAY_NAME + " != '' ))";
-
         return new CursorLoader(this, MyContentProvider.CONTENT_URI,
                 ITEMS_SUMMARY_PROJECTION, null, null,
                 null);
@@ -153,8 +122,10 @@ public class MyListActivity extends ListActivity
     @Override
     public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
         getContentResolver()
-                .delete(MyContentProvider.CONTENT_URI,MyContentProvider.ITEM_ID+"=?",new String[]{""+id});
+                .delete(MyContentProvider.CONTENT_URI, MyContentProvider.ITEM_ID + "=?", new String[]{"" + id});
+        getLoaderManager().restartLoader(0,null,this);
         Log.d(TAG,"onItemLongClick fired");
         return true;
     }
+
 }
